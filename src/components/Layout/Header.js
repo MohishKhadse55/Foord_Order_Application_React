bfe13a58f83890a8a5bc758fc80e9d0aa906ccd4
Mohish Khadse <mohishkhadse3222@gmail.com>
2022-10-09 03:15:21 +0530

    Initial commit

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,21 @@
+// import Fragment from 'react';
+
+import { Fragment } from 'react';
+import mealImage from '../assets/meals.jpg';
+import classes from './Header.module.css';
+import HeaderCartButton from './HeaderCartButton';
+const Header = (props) => {
+  return (
+    <Fragment>
+      <header className={classes.header}>
+        <h1>React Meal</h1>
+        <HeaderCartButton />
+      </header>
+      <div className={classes['main-image']}>
+        <img src={mealImage} alt="a table of food" />
+      </div>
+    </Fragment>
+  );
+};
+
+export default Header;
---
+++ src/components/Meals/AvailableMeals.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import classes from './AvailabelMeals.module.css';
+import Card from '../UI/Card';
+import MealItems from './MealItems/MealItems';
+
+const DUMMY_MEALS = [
+  {
+    id: 'm1',
+    name: 'Sushi',
+    description: 'Finest fish and veggies',
+    price: 22.99,
+  },
+  {
+    id: 'm2',
+    name: 'Schnitzel',
+    description: 'A german specialty!',
+    price: 16.5,
+  },
+  {
+    id: 'm3',
+    name: 'Barbecue Burger',
+    description: 'American, raw, meaty',
+    price: 12.99,
+  },
+  {
+    id: 'm4',
+    name: 'Green Bowl',
+    description: 'Healthy...and green...',
+    price: 18.99,
+  },
+]; 
+
+function AvailableMeals(props) {
+  const meallist = DUMMY_MEALS.map((meal) => (
+    <MealItems
+      {/*  here insteal of passing each value as seperately you can pass the meal directly and and can get the attributes using props.meal.name */}
+      key={meal.id}
+      name={meal.name}
+      description={meal.description}
+      price={meal.price}
+    />
+  ));
+  return (
+    <section className={classes.meals}>
+      <Card>
+        <ul>{meallist}</ul>
+      </Card>
+    </section>
+  );
+}
+
+export default AvailableMeals;
